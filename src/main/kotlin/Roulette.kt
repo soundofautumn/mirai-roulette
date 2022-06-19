0b60f6591f21182d022aa9ec265da94ef79826ae
AutumnSound <qjm1412@163.com>
2022-06-19 10:39:58 +0800

    新增模式切换

--- src/main/kotlin/Roulette.kt
+++ src/main/kotlin/Roulette.kt
@@ -4,9 +4,7 @@ import net.mamoe.mirai.console.data.AutoSavePluginConfig
 import net.mamoe.mirai.console.data.value
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
-import net.mamoe.mirai.contact.Member
-import net.mamoe.mirai.contact.PermissionDeniedException
-import net.mamoe.mirai.contact.nameCardOrNick
+import net.mamoe.mirai.contact.*
 import net.mamoe.mirai.event.GlobalEventChannel
 import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.message.data.At
@@ -38,6 +36,11 @@ object RouletteConfig : AutoSavePluginConfig("roulette") {
     val quotations: MutableList<String> by value()
 }
 
+enum class RouletteType(val OperateName: String, val introduction: String) {
+    MUTE("禁言", "禁言"),
+    KICK("踢人", "踢出"),
+}
+
 object Roulette : KotlinPlugin(
     JvmPluginDescription(
         id = "per.autumn.mirai.roulette",
@@ -45,6 +48,8 @@ object Roulette : KotlinPlugin(
     )
 ) {
     private val dataMap = HashMap<Long, GroupRouletteData>()
+
+    private var rouletteType: RouletteType = RouletteType.MUTE
 
     //默认的回复语录
     //来自 https://github.com/InvoluteHell/Pallas-Bot/blob/master/src/plugins/roulette/__init__.py
@@ -56,7 +61,7 @@ object Roulette : KotlinPlugin(
         "哭嚎吧，为你们不堪一击的信念。",
         "现在可没有后悔的余地了。",
         //轮盘介绍  5
-        "这是一把充满荣耀与死亡的左轮手枪，六个弹槽只有一颗子弹，中弹的那个人将会被禁言。勇敢的战士们啊，扣动你们的扳机吧！",
+        "这是一把充满荣耀与死亡的左轮手枪，六个弹槽只有$1颗子弹，中弹的那个人将会被$2。勇敢的战士们啊，扣动你们的扳机吧！",
         //手枪卡壳（5%的概率开枪失败）  6
         "我的手中的这把武器，找了无数工匠都难以修缮如新。不......不该如此......",
         //打中时的回复  7
@@ -69,15 +74,41 @@ object Roulette : KotlinPlugin(
         RouletteConfig.reload()
         init()
         GlobalEventChannel.filterIsInstance<GroupMessageEvent>()
+            .filter { it.permission.isOperator() }
+            .subscribeAlways<GroupMessageEvent> {
+                val msg = it.message.contentToString()
+                RouletteType.values()
+                    .forEach { type ->
+                        if (msg == RouletteConfig.botName.ifEmpty { it.bot.nameCardOrNick } + type.OperateName + "轮盘") {
+                            rouletteType = type
+                            it.group.sendMessage("轮盘已切换至${rouletteType.OperateName}模式")
+                            return@forEach
+                        }
+                    }
+            }
+        GlobalEventChannel.filterIsInstance<GroupMessageEvent>()
             .filter {
                 it.message.contentToString().startsWith(RouletteConfig.botName.ifEmpty { it.bot.nameCardOrNick } + "轮盘")
             }
             .subscribeAlways<GroupMessageEvent> {
                 val lastChar = it.message.contentToString().trim().last()
                 val bulletCount: Int = if (lastChar.isDigit()) lastChar.toString().toInt() else 1
+                logger.debug("$bulletCount $lastChar")
                 if (!dataMap.containsKey(it.group.id)) {
                     dataMap[it.group.id] = GroupRouletteData(bulletCount)
-                    it.group.sendMessage(RouletteConfig.quotations[5].getMessageWithAt(it.sender))
+                    it.group.sendMessage(
+                        RouletteConfig.quotations[5].replace(
+                            "$1", when (bulletCount) {
+                                1 -> "一"
+                                2 -> "二"
+                                3 -> "三"
+                                4 -> "四"
+                                5 -> "五"
+                                6 -> "六"
+                                else -> ""
+                            }
+                        ).replace("$2", rouletteType.introduction).getMessageWithAt(it.sender)
+                    )
                 }
             }
         GlobalEventChannel.filterIsInstance<GroupMessageEvent>()
@@ -90,10 +121,17 @@ object Roulette : KotlinPlugin(
                         it.group.sendMessage(RouletteConfig.quotations[6].getMessageWithAt(it.sender))
                     } else {
                         try {
-                            it.sender.mute((1..20).random() * 60)
+                            when (rouletteType) {
+                                RouletteType.MUTE -> it.sender.mute((1..20).random() * 60)
+                                RouletteType.KICK -> (it.sender as NormalMember).kick("")
+                            }
                             it.group.sendMessage(RouletteConfig.quotations[7].getMessageWithAt(it.sender))
-                        } catch (e: PermissionDeniedException) {
-                            it.group.sendMessage(RouletteConfig.quotations[8].getMessageWithAt(it.sender))
+                        } catch (e: Exception) {
+                            when (e) {
+                                is PermissionDeniedException, is ClassCastException ->
+                                    it.group.sendMessage(RouletteConfig.quotations[8].getMessageWithAt(it.sender))
+                                else -> throw e
+                            }
                         }
                     }
                     dataMap.remove(it.group.id)
